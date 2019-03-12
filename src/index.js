7714d901ce0df7d9f00f9196f9f48ec943bfd486
Andrew Lisowski <lisowski54@gmail.com>
2019-03-11 20:34:28 -0700

    update deps

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import React from 'react';
+import PropTypes from 'prop-types';
 import NextLink from 'next/link';
 import getConfig from 'next/config';
 
@@ -9,6 +10,12 @@ export const prefixURL = url => path.join(publicRuntimeConfig.assetPrefix, url);
 
 export const Image = props => <img {...props} src={prefixURL(props.src)} />;
 
+Image.propTypes = {
+  src: PropTypes.string.isRequired
+};
+
 export const Link = props => (
   <NextLink {...props} as={prefixURL(props.as || props.to || props.href)} />
 );
+
+Link.propTypes = NextLink.propTypes;
